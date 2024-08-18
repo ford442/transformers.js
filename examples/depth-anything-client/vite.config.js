1723da5c4b1247cc8c788ac7e83185fefb0b8ded
Noah Cohn <noahc42@gmail.com>
2024-08-18 15:21:28 -0700

    Update vite.config.js

--- examples/depth-anything-client/vite.config.js
+++ examples/depth-anything-client/vite.config.js
@@ -2,5 +2,11 @@ import { defineConfig } from 'vite';
 export default defineConfig({
   build: {
     target: 'esnext'
-  }
+  },
+   rollupOptions: {
+      input: {
+        main: './main.js',  // First entry point
+        secondary: './loader.js' // Second entry point
+      }
+    }
 });
