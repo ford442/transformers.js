801dee76f99b950c157a3449638aa7ba6fc90498
Noah Cohn <noahc42@gmail.com>
2024-08-18 16:01:48 -0700

    Update vite.config.js

--- examples/depth-anything-client/vite.config.js
+++ examples/depth-anything-client/vite.config.js
@@ -2,5 +2,9 @@ import { defineConfig } from 'vite';
 export default defineConfig({
   build: {
     target: 'esnext'
-  }
+  },
+    resolve: {
+    alias: {
+      'three/addons/loaders/GLTFLoader.js': 'three/examples/jsm/loaders/GLTFLoader.js'
+    }
 });
