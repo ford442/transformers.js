b7eebb36a77e5ca9edcaad77845740c1efaad02c
Noah Cohn <noahc42@gmail.com>
2024-08-20 22:44:40 -0700

    Update main.js

--- examples/depth-anything-client/main.js
+++ examples/depth-anything-client/main.js
@@ -294,13 +294,13 @@ console.error(error);
 }
 
 function animate() {
- /*
+
 const time = performance.now();
 const delta = ( time - prevTime ) / 1000;
 velocity.x -= velocity.x * 10.0 * delta;
 velocity.z -= velocity.z * 10.0 * delta;
 velocity.y -= 9.8 * 100.0 * delta;
-*/
+ /*
 direction.z = Number( moveForward ) - Number( moveBackward );
 direction.x = Number( moveRight ) - Number( moveLeft );
 direction.normalize(); // this ensures consistent movements in all directions
@@ -310,7 +310,7 @@ controlsL.moveRight( -velocity.x * delta );
 controlsL.moveForward( -velocity.z * delta );
 controlsL.getObject().position.y += velocity.y * delta; 
 prevTime = time;
-
+*/
 requestAnimationFrame( animate );
 rendererL.render( sceneL, cameraL );
 }
