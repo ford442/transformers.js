904d785495013926f64863a590f1ddb5abac90c4
Noah Cohn <noahc42@gmail.com>
2024-08-21 00:17:43 -0700

    Update main.js

--- examples/depth-anything-client/main.js
+++ examples/depth-anything-client/main.js
@@ -357,11 +357,11 @@ function animate() {
 
     // Calculate movement direction based on camera's orientation
    // direction.copy(forward).multiplyScalar(Number(moveForward) - Number(moveBackward));
-    direction.add(right).multiplyScalar(Number(moveRight) - Number(moveLeft));
+   // direction.add(right).multiplyScalar(Number(moveRight) - Number(moveLeft));
     direction.normalize(); 
 
-  if (moveForward || moveBackward) velocity.z -= direction.z * 10.0 * delta;
-  if (moveLeft || moveRight) velocity.x -= direction.x * 10.0 * delta;   
+  if (moveForward || moveBackward) velocity.z -= direction.z * 5.0 * delta;
+  if (moveLeft || moveRight) velocity.x -= direction.x * 5.0 * delta;   
 
   // Directly update the camera's position based on velocity and delta time
   controlsL.getObject().position.x -= velocity.x * delta;
