b1cb9f2f93f4943642c4b64c7e8c11ed4b8bff44
Noah Cohn <noahc42@gmail.com>
2024-08-20 22:46:15 -0700

    Update main.js

--- examples/depth-anything-client/main.js
+++ examples/depth-anything-client/main.js
@@ -304,9 +304,10 @@ velocity.y -= 9.8 * 100.0 * delta;
 direction.z = Number( moveForward ) - Number( moveBackward );
 direction.x = Number( moveRight ) - Number( moveLeft );
 direction.normalize(); // this ensures consistent movements in all directions
- /*
+
 if ( moveForward || moveBackward ) velocity.z -= direction.z * 400.0 * delta;
 if ( moveLeft || moveRight ) velocity.x -= direction.x * 400.0 * delta;
+ /*
 controlsL.moveRight( -velocity.x * delta );
 controlsL.moveForward( -velocity.z * delta );
 controlsL.getObject().position.y += velocity.y * delta; 
