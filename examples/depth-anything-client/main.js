919730910ea44058a614d9ac6f3448d5aa28ecd0
Noah Cohn <noahc42@gmail.com>
2024-09-03 05:49:51 -0700

    Update main.js

--- examples/depth-anything-client/main.js
+++ examples/depth-anything-client/main.js
@@ -26,7 +26,7 @@ const fileUpload = document.getElementById('upload');
 const imageContainer = document.getElementById('container');
 const example = document.getElementById('example');
 status.textContent = 'Loading model...';
-const depth_estimator = await pipeline('depth-estimation', 'Xenova/depth-anything-small-hf',{dtype: 'fp32',backend: 'webgpu'});
+const depth_estimator = await pipeline('depth-estimation', 'Xenova/depth-anything-small-hf',{dtype: 'fp32',device: 'webgpu'});
 
 	status.textContent = 'Ready';
 const channel = new BroadcastChannel('imageChannel');
