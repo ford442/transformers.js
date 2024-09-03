644bd9af3b240a89ee8b5f3b6b3c798aea9e8d28
Noah Cohn <noahc42@gmail.com>
2024-09-03 13:13:40 -0700

    Update main.js

--- examples/depth-anything-client/main.js
+++ examples/depth-anything-client/main.js
@@ -165,10 +165,13 @@ imgDataD[i+2]-=disData;
 // const texture16 = new THREE.DataTexture(data16, imgData.width, imgData.height, THREE.RGBAFormat, THREE.HalfFloatType);
 // texture16.internalFormat = 'RGBA16F';
 // texture16.needsUpdate = true;
+const texture8 = new THREE.DataTexture(displaceData, imgData.width, imgData.height, THREE.RGBAIntegerFormat, THREE.UnsignedIntType);
+texture8.internalFormat = 'RGBA8UI';
+
 const displace2= new THREE.CanvasTexture(displaceData);
-material.displacementMap=displace2;
-material.roughness=.75;
-material.metalness=.15;
+material.displacementMap=texture8;
+material.roughness=.85;
+material.metalness=.05;
 // material.roughnessMap=image;
         //  bump map
 
