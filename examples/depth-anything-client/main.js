819deff8f3ce0663bb93a11ae36b8efd9fa72227
Noah Cohn <noahc42@gmail.com>
2024-09-03 02:36:34 -0700

    Update main.js

--- examples/depth-anything-client/main.js
+++ examples/depth-anything-client/main.js
@@ -267,8 +267,8 @@ plane.position.y = wobbleAmount * Math.cos(time * 3.13 * 1.5); // More variation
 plane.rotation.z = wobbleAmount * 0.315 * Math.cos(time * wobbleSpeed * 0.578); 
 camera.position.x = wobbleAmount * Math.sin(time * 1.77777);
 camera.position.y = wobbleAmount * Math.cos(time * 3 * 1.11115); // More variation in y-axis frequency
-camera.position.z = wobbleAmount * 0.13 * Math.sin(time * wobbleSpeed * 0.777); // Add some z-axis movement
-camera.rotation.z = 0.0515 * Math.cos(time * wobbleSpeed * 0.778); 
+// camera.position.z = wobbleAmount * 0.13 * Math.sin(time * wobbleSpeed * 0.777); // Add some z-axis movement
+// camera.rotation.z = 0.0515 * Math.cos(time * wobbleSpeed * 0.778); 
 // camera.lookAt(scene.position); // Make the camera look at the center
 	
 spotLight1.position.x *= Math.cos( time ) * .15;
