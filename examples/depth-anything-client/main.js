c505c458c2cd4ff88d26b470833a521a7d5dfe3b
Noah Cohn <noahc42@gmail.com>
2024-09-03 02:19:37 -0700

    Update main.js

--- examples/depth-anything-client/main.js
+++ examples/depth-anything-client/main.js
@@ -91,7 +91,7 @@ const camera = new THREE.PerspectiveCamera(30, width / height, 0.01, 10);
 camera.position.z = 2;
 scene.add(camera);
 
-// const renderer = new THREE.WebGPURenderer({ canvas:canvas,antialias:true });
+// const renderer = new THREE.WebGPURenderer({ canvas:canvas });
 const renderer = new THREE.WebGLRenderer({ canvas, antialias: true,premultipliedAlpha:false });
 
 renderer.autoClear = false;
@@ -263,8 +263,8 @@ plane.position.x = wobbleAmount * Math.sin(time * wobbleSpeed);
 plane.position.y = wobbleAmount * Math.cos(time * 3.13 * 1.5); // More variation in y-axis frequency
 // camera.position.z = wobbleAmount * 0.13 * Math.sin(time * wobbleSpeed * 0.777); // Add some z-axis movement
 plane.rotation.z = wobbleAmount * 0.315 * Math.cos(time * wobbleSpeed * 0.578); 
-camera.position.x = wobbleAmount * Math.sin(time * 1.77777);
-camera.position.y = wobbleAmount * Math.cos(time * 3 * 1.11115); // More variation in y-axis frequency
+   // camera.position.x = wobbleAmount * Math.sin(time * 1.77777);
+   // camera.position.y = wobbleAmount * Math.cos(time * 3 * 1.11115); // More variation in y-axis frequency
 // camera.position.z = wobbleAmount * 0.13 * Math.sin(time * wobbleSpeed * 0.777); // Add some z-axis movement
   // camera.rotation.z = wobbleAmount * 0.515 * Math.cos(time * wobbleSpeed * 0.778); 
 // camera.lookAt(scene.position); // Make the camera look at the center
