a136584d4ac50aff44190ae11c98ab4a51172d0a
Noah Cohn <noahc42@gmail.com>
2024-09-03 01:55:09 -0700

    Update main.js

--- examples/depth-anything-client/main.js
+++ examples/depth-anything-client/main.js
@@ -90,7 +90,7 @@ const camera = new THREE.PerspectiveCamera(30, width / height, 0.01, 10);
 camera.position.z = 2;
 scene.add(camera);
 
-const renderer = new THREE.WebGPURenderer({ canvas });
+const renderer = new THREE.WebGPURenderer({ canvas:canvas,antialias:true });
 // const rendererW = new THREE.WebGPU({ canvas, antialias: true,premultipliedAlpha:false });
 
 renderer.autoClear = false;
