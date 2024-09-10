d664e3605751566d0f62b4851b470e939529b6f6
Noah Cohn <noahc42@gmail.com>
2024-09-10 13:23:02 -0700

    Update main.js

--- examples/depth-anything-client/main.js
+++ examples/depth-anything-client/main.js
@@ -31,7 +31,7 @@ const imageContainer = document.getElementById('container');
 const example = document.getElementById('example');
 status.textContent = 'Loading model...';
 
-const depth_estimator = await pipeline('depth-estimation', 'Xenova/depth-anything-small-hf',{device:'webgpu'});
+const depth_estimator = await pipeline('depth-estimation', 'Xenova/depth-anything-small-hf',{device:'wasm'});
 
 status.textContent = 'Ready';
 const channel = new BroadcastChannel('imageChannel');
