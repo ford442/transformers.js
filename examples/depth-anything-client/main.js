026b2e204490d488e28cd10708ef0a679c5ea803
Noah Cohn <noahc42@gmail.com>
2024-09-18 21:36:52 -0700

    Update main.js

--- examples/depth-anything-client/main.js
+++ examples/depth-anything-client/main.js
@@ -265,6 +265,55 @@ scene.add(plane);
 	//fog
 // scene.tfog = new THREE.Fog( 0x6f00a0, 0.1, 10 );
 
+//  smoke
+
+	
+const map = textureLoader.load( './smoke1.png' );
+
+// create nodes
+const lifeRange = range( .1, 1 );
+const offsetRange = range( new THREE.Vector3( - 2, 3, - 2 ), new THREE.Vector3( 2, 5, 2 ) );
+const timer = timerLocal( .2, 1/*100000*/ ); // @TODO: need to work with 64-bit precision
+const lifeTime = timer.mul( lifeRange ).mod( 1 );
+const scaleRange = range( .3, 2 );
+const rotateRange = range( .1, 4 );
+const life = lifeTime.div( lifeRange );
+const fakeLightEffect = positionLocal.y.oneMinus().max( 0.2 );
+const textureNode = texture( map, rotateUV( uv(), timer.mul( rotateRange ) ) );
+const opacityNode = textureNode.a.mul( life.oneMinus() );
+const smokeColor = mix( color( 0x2c1501 ), color( 0x222222 ), positionLocal.y.mul( 3 ).clamp() );
+
+// create particles
+
+const smokeNodeMaterial = new THREE.SpriteNodeMaterial();
+smokeNodeMaterial.colorNode = mix( color( 0xf27d0c ), smokeColor, life.mul( 2.5 ).min( 1 ) ).mul( fakeLightEffect );
+smokeNodeMaterial.opacityNode = opacityNode;
+smokeNodeMaterial.positionNode = offsetRange.mul( lifeTime );
+smokeNodeMaterial.scaleNode = scaleRange.mul( lifeTime.max( 0.3 ) );
+smokeNodeMaterial.depthWrite = false;
+smokeNodeMaterial.transparent = true;
+
+const smokeInstancedSprite = new THREE.Mesh( new THREE.PlaneGeometry( 1, 1 ), smokeNodeMaterial );
+smokeInstancedSprite.scale.setScalar( 400 );
+smokeInstancedSprite.count = 2000;
+scene.add( smokeInstancedSprite );
+
+const fireNodeMaterial = new THREE.SpriteNodeMaterial();
+fireNodeMaterial.colorNode = mix( color( 0xb72f17 ), color( 0xb72f17 ), life );
+fireNodeMaterial.positionNode = range( new THREE.Vector3( - 1, 1, - 1 ), new THREE.Vector3( 1, 2, 1 ) ).mul( lifeTime );
+fireNodeMaterial.scaleNode = smokeNodeMaterial.scaleNode;
+fireNodeMaterial.opacityNode = opacityNode;
+fireNodeMaterial.blending = THREE.AdditiveBlending;
+fireNodeMaterial.transparent = true;
+fireNodeMaterial.depthWrite = false;
+
+const fireInstancedSprite = new THREE.Mesh( new THREE.PlaneGeometry( 1, 1 ), fireNodeMaterial );
+fireInstancedSprite.scale.setScalar( 400 );
+fireInstancedSprite.count = 100;
+fireInstancedSprite.position.y = - 100;
+fireInstancedSprite.renderOrder = 1;
+scene.add( fireInstancedSprite );
+	
 // Create Spotlights
 const spotLight1 = new THREE.SpotLight(0x2217de, 34.420)
 spotLight1.position.set(0, 1.38, 0.181)
