c24be95cb377b8c6e9c190a180c9968955b06226
Noah Cohn <noahc42@gmail.com>
2024-09-19 21:21:02 -0700

    Update main.jsx

--- examples/depth-anything-client/main.jsx
+++ examples/depth-anything-client/main.jsx
@@ -266,28 +266,25 @@ scene.add(plane);
 const particleGeometry = new THREE.BufferGeometry();
 const particleMaterial = new THREE.PointsMaterial({
 color: 0xaaaaaa,
-size: 0.05,
+size: 0.005,
 map: new THREE.TextureLoader().load('./smoke1.png'), // Load a smoke texture
 blending: THREE.AdditiveBlending,
 depthWrite: false,
 transparent: true,
 });
-
+particleMaterial.receiveShadow=1;
 const particles = new THREE.Points(particleGeometry, particleMaterial);
 scene.add(particles);
 const particleCount = 10000;
 const positions = new Float32Array(particleCount * 3);
-
 for (let i = 0; i < particleCount; i++) {
-  const x = (Math.random() - 0.5) * 2;
-  const y = (Math.random() - 0.5) * 2;
-  const z = (Math.random() - 0.5) * 2;
-
-  positions[i * 3] = x;
-  positions[i * 3 + 1] = y;
-  positions[i * 3 + 2] = z;
+const x = (Math.random() - 0.5) * 2;
+const y = (Math.random() - 0.5) * 2;
+const z = (Math.random() - 0.5) * 2;
+positions[i * 3] = x;
+positions[i * 3 + 1] = y;
+positions[i * 3 + 2] = z;
 }
-
 particleGeometry.setAttribute('position', new THREE.BufferAttribute(positions, 3));
 
 	
