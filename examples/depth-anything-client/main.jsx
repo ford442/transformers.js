9f5a93ea9730438e44a7019ab723fe0a1aa2fda5
Noah Cohn <noahc42@gmail.com>
2024-10-30 23:01:06 -0700

    Update main.jsx

--- examples/depth-anything-client/main.jsx
+++ examples/depth-anything-client/main.jsx
@@ -339,8 +339,6 @@ const tmpdpt=document.querySelector('#pydepth');
 tmpdpt.width=image.width;
 tmpdpt.height=image.height;
 	  //  get a copy of the depth map for HTML/pyodide
-
-	
 ctx.putImageData(depthData, 0, 0);
 const depthDataUrl = exportCanvas.toDataURL('image/jpeg', 1.0);
 tmpdpt.src=depthDataUrl;
