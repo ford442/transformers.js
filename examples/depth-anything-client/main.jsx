7afff5e3a040c144c5a9f6fcbb64855f8ef246d6
Noah Cohn <noahc42@gmail.com>
2024-11-04 22:45:54 -0800

    Update main.jsx

--- examples/depth-anything-client/main.jsx
+++ examples/depth-anything-client/main.jsx
@@ -263,7 +263,7 @@ function denormalize(value, oldMin, oldMax, newMin, newMax) {
 }
 
 async function inpaintImage() {
-  const inpaintingSession = await InferenceSession.create('https://noahcohn.com/model/model_float32.onnx',{ executionProviders: ["wasm"] });
+  const inpaintingSession = await InferenceSession.create('https://noahcohn.com/model/model_float32.onnx');
 console.log('got ORT session');
 	const inputCanvas = document.getElementById('dvi1');
   const maskCanvas = document.getElementById('dvi4');
