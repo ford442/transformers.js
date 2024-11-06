d36b6fe68c047b0de08579e7534c59379b1dc550
Noah Cohn <noahc42@gmail.com>
2024-11-05 22:47:38 -0800

    Update main.jsx

--- examples/depth-anything-client/main.jsx
+++ examples/depth-anything-client/main.jsx
@@ -21,8 +21,8 @@ import { LoopSubdivision } from 'three-subdivide';
 // import { toPng, toJpeg, toBlob, toPixelData, toSvg } from 'html-to-image';
 
 env.allowLocalModels = false;
-env.backends.onnx.wasm.proxy = true;
-env.backends.onnx.wasm.numThreads = 4;
+env.backends.onnx.wasm.proxy = false;
+env.backends.onnx.wasm.numThreads = 1;
 env.backends.onnx.wasm.simd = true;
  
 const DEFAULT_SCALE = 0.2713;
@@ -35,7 +35,7 @@ status.textContent = 'Loading model...';
 // const depth_estimator = await pipeline('depth-estimation', 'onnx-community/depth-anything-v2-large', { dtype: 'fp32', device: 'webgpu' });
 // const depth_estimator = await pipeline('depth-estimation', 'Xenova/depth-anything-large-hf', { dtype: 'fp16', device: 'webgpu' });
 // const depth_estimator = await pipeline('depth-estimation', 'Xenova/depth-anything-base-hf', { dtype: 'fp16', device: 'webgpu' });
-const depth_estimator = await pipeline('depth-estimation', 'Xenova/depth-anything-small-hf',{dtype:'fp32',device:'webgpu'});
+const depth_estimator = await pipeline('depth-estimation', 'Xenova/depth-anything-large-hf',{dtype:'fp32',device:'webgpu'});
 
 status.textContent = 'Ready';
 const channel = new BroadcastChannel('imageChannel');
